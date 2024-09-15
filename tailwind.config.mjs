515bb0dee9d38264ba8328b2236a96047d93f3b3
Debjit Biswas <debjit@fastmail.fm>
2024-09-15 22:22:54 +0530

    refactor: Add astro and alpinejs

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,svg,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'selector',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
